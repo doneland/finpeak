5573f934d1170f5644be1a12611d2a5e52b97479
doneland <doneland@em-mac.local>
2016-08-21 09:57:03 +0300

    Initial commit

---
+++ client/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Router} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes from 'routes';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from '../shared/reducers';
+import { fromJS } from 'immutable';
+
+const history = createBrowserHistory();
+let initialState = window.__INITIAL_STATE__;
+console.log('Initial state:', initialState);
+
+// Transform into Immutable.js collections,
+// but leave top level keys untouched for Redux
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+   });
+
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
+
+render(
+  <Provider store={store}>
+      <Router children={routes} history={history} />
+  </Provider>,
+  document.getElementById('react-view')
+);
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import server from './server';
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log('Server listening on', PORT);
+});
